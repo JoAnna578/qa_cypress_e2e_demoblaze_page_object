39da657c6d85b037be509f597ee85050c7e3c7b8
JoAnna578 <joannakoloczek1@gmail.com>
2025-10-25 10:26:40 +0200

    fix(pom): unify PageObject classes and make checkout flow atomic

--- cypress/support/PageObject.js
+++ cypress/support/PageObject.js
@@ -1,13 +1,67 @@
+// PageObject.js
 class PageObject {
   visit(url) {
     cy.visit(url || this.url);
   }
 
-  assertAllert(alertMessage) {
+  assertAlert(alertMessage) {
     cy.on('window:alert', (alert) => {
       expect(alert).to.eq(alertMessage);
     });
   }
 }
 
-export default PageObject;
+class HomePage extends PageObject {
+  clickCategory(name) {
+    cy.contains('.list-group a', name).click();
+  }
+
+  clickProduct(name) {
+    cy.contains('.card-title a', name).click();
+  }
+
+  navigateToCart() {
+    cy.contains('a', 'Cart').click();
+  }
+}
+
+class ProductPage extends PageObject {
+  addToCart() {
+    cy.contains('a', 'Add to cart').click();
+  }
+}
+
+class CartPage extends PageObject {
+  assertProductInCart(name) {
+    cy.get('#tbodyid').contains('td', name).should('be.visible');
+  }
+
+  placeOrder() {
+    cy.contains('button', 'Place Order').click();
+  }
+
+  fillOrderForm(data) {
+    cy.get('#name').type(data.name);
+    cy.get('#country').type(data.country);
+    cy.get('#city').type(data.city);
+    cy.get('#card').type(data.card);
+    cy.get('#month').type(data.month);
+    cy.get('#year').type(data.year);
+  }
+
+  purchase() {
+    cy.contains('button', 'Purchase').click();
+  }
+
+  assertPurchaseData(data) {
+    cy.get('.sweet-alert').should('contain.text', data.name);
+    cy.get('.sweet-alert').should('contain.text', data.country);
+    cy.get('.sweet-alert').should('contain.text', data.card);
+  }
+
+  confirmOk() {
+    cy.contains('button', 'OK').click();
+  }
+}
+
+export { PageObject, HomePage, ProductPage, CartPage };
