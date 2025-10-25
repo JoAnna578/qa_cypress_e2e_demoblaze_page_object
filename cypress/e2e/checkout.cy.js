9690edef2b1e8b41ef17b50be5b89bc734140ae3
JoAnna578 <joannakoloczek1@gmail.com>
2025-10-25 10:08:04 +0200

    feat(pom): implement Demoblaze checkout flow with Page Object Model

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,11 +1,43 @@
 /// <reference types='cypress' />
 
-describe('', () => {
-  before(() => {
+const HomePage = require('../pages/HomePage');
+const ProductPage = require('../pages/ProductPage');
+const CartPage = require('../pages/CartPage');
 
+describe('Demoblaze Checkout Flow', () => {
+  const homePage = new HomePage();
+  const productPage = new ProductPage();
+  const cartPage = new CartPage();
+
+  before(() => {
+    homePage.visit();
   });
 
-  it('', () => {
+  it('should add Sony vaio i7 to cart and verify alert', () => {
+    homePage.clickCategory('Laptops');
+    homePage.clickProduct('Sony vaio i7');
+    productPage.addToCart();
+    productPage.assertAlertMessage('Product added.');
+  });
 
+  it('should place order and verify purchase', () => {
+    cy.contains('a', 'Cart').click();
+    cartPage.assertProductInCart('Sony vaio i7');
+
+    cartPage.placeOrder();
+
+    const orderData = {
+      name: 'John Doe',
+      country: 'USA',
+      city: 'New York',
+      card: '1234567890123456',
+      month: '12',
+      year: '2025'
+    };
+
+    cartPage.fillOrderForm(orderData);
+    cartPage.purchase();
+    cartPage.assertPurchaseData(orderData);
+    cartPage.confirmOk();
   });
 });
